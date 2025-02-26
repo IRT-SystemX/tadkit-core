3fd7027e3383c46f7199db90083ab6e406b6c834
yann.gaste <yann.gaste@irt-systemx.fr>
2025-02-26 17:53:52 +0100

    fix sphinx css on github pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,16 +5,29 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 project = "tadkit"
 copyright = "2025, IRT-SystemX"
 author = "IRT-SystemX"
 release = "1.0"
 
+import sphinx_rtd_theme
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+
+extensions = [
+        'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme']
+
+
+# Add the theme's static files (this step is usually optional)
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 templates_path = ["_templates"]
 exclude_patterns = []
