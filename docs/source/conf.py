1a18f203ac1b64e4057e842099a38e3ca3fddb02
yann.gaste <yann.gaste@irt-systemx.fr>
2025-02-26 23:34:34 +0100

    fix sphinx css on github pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,10 @@ copyright = "2025, IRT-SystemX"
 author = "IRT-SystemX"
 release = "1.0"
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath(f"{__file__}/../../.."))
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
