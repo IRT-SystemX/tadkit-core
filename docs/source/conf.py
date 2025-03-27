598a3bb3e190895462de7f51e92595117a667bc0
yann.gaste <yann.gaste@irt-systemx.fr>
2025-03-27 17:25:24 +0100

    docs: update sphinx content

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,41 +1,42 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# conf.py
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "tadkit"
-copyright = "2025, IRT-SystemX"
-author = "IRT-SystemX"
-release = "1.0"
-
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(f"{__file__}/../../.."))
 
+
+project = 'TADKit'
+copyright = '2025, IRT-SystemX'
+author = 'Martin Royer'
+release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-exclude_patterns = [
-    '_build',
-    'Thumbs.db',
-    '.DS_Store',
-]
-
 extensions = [
-    "sphinx.ext.githubpages"
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    #"myst_parser",
+    "myst_nb",
+    #"nbsphinx",
+    "sphinx.ext.githubpages",
 ]
 
-templates_path = ["_templates"]
-exclude_patterns = []
+nb_execution_mode = "off"
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
-
-
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static', 'images']
